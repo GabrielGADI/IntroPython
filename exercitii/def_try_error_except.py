afada6dc0c8efd48d145242a00fb29ef448f2a71
GabrielGADI <gabrieldomiteanu@yahoo.com>
2023-06-19 16:34:37 +0300

    exercitii

--- exercitii/def_try_error_except.py
+++ exercitii/def_try_error_except.py
@@ -86,3 +86,50 @@ print(next(gen))  # a doua cifra
 print(list(gen))  # urmatoarele cifre fara ultima in lista
 
 print(20 * " - ", "Exercitiul 7", 20 * " - ")
+
+# afiseaza suma
+def func(x, y, z):
+    return x + y + z
+
+
+params = [1, 2, 3]
+result = func(*params)
+
+print(result)
+
+print(20 * " - ", "Exercitiul 8", 20 * " - ")
+
+# type error
+# x = 5
+# y = "2"
+# result = x + y
+# print(result) # TypeError
+
+print(20 * " - ", "Exercitiul 9", 20 * " - ")
+
+# IndexError
+# nums = [1, 2, 3, 4, 5]
+# for i in range(len(nums)):
+#     if nums[i] == 3:
+#         nums.pop(i)
+# print(nums)
+
+print(20 * " - ", "Exercitiul 10 IndexError", 20 * " - ")
+
+# TypeError
+# def func(a, b):
+#      return a * b
+# result = func(2, 3)
+# result = result(4, 5)
+#
+# print(result)
+print(20 * " - ", "Exercitiul 11 TypeError", 20 * " - ")
+
+# IndexError - nu avem al 5-lea element in lista
+# nums = [1, 2, 3, 4, 5]
+# squares = [num**2 for num in nums]
+# print(squares[5])
+#
+
+
+print(20 * " - ", "Exercitiul 12 IndexError", 20 * " - ")
